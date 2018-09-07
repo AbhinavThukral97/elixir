ba73e1ef8cf1f7cd9b98747ee524467c71ed8769
Abhinav Thukral <thukral.abhinav@gmail.com>
2018-09-07 22:52:52 +0530

    first-commit

---
+++ js/master.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  $('.logo').css('transform','scale(1)');
+  setTimeout(function(){
+    $('h1').css('top','0px');
+    $('h1').css('opacity','1');
+  },500);
+  setTimeout(function(){
+    $('h2').css('top','0px');
+    $('h2').css('opacity','1');
+  },750);
+  $(window).scroll(function(){
+   i = $(window).scrollTop();
+   if(i>0)
+    $('header').addClass('shadow');
+  else
+    $('header').removeClass('shadow');
+  });
+  // console.log('Running');
+  // $(window).scroll(function(){
+  //   let x = $(window).scrollTop();
+  //   $('section.intro').css('background-position','center '+ x/4 + 'px');
+  //   if(x>0){
+  //     $('header').addClass('go-white');
+  //   } else {
+  //     $('header').removeClass('go-white');
+  //   }
+  // });
+
+  // $('.scrollicon-mouse').click(function(){
+  //   var  i = $('.about').offset().top;
+  //   $('body, html').animate({scrollTop: i - 70}, 1000);
+  // });
+
+  // $('.home-link').click(function(){
+  //   $('body, html').animate({scrollTop: 0}, 1000);
+  // });
+
+  // $('.about-link').click(function(){
+  //   var  i = $('.about').offset().top;
+  //   $('body, html').animate({scrollTop: i - 70}, 1000);
+  // });
+
+  // $('.features-link').click(function(){
+  //   var  i = $('.features').offset().top;
+  //   $('body, html').animate({scrollTop: i - 70}, 1000);
+  // });
+
+  // $('.support-link').click(function(){
+  //   var  i = $('.support').offset().top;
+  //   $('body, html').animate({scrollTop: i - 70}, 1000);
+  // });
+});
