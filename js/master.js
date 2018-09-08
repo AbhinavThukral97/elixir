4c25826464402547dd2e1b50972914922431f368
Abhinav Thukral <thukral.abhinav@gmail.com>
2018-09-08 10:40:28 +0530

    responsive

--- js/master.js
+++ js/master.js
@@ -19,28 +19,64 @@ $(document).ready(function(){
     $('header').addClass('shadow');
   else
     $('header').removeClass('shadow');
-    $('.activities-panel').css('background-position','center ' + (a-i-h)/5 +'px');
-    $('.join-panel').css('background-position','center ' + (j-i-h)/5 +'px');
+    // $('.activities-panel').css('background-position','center ' + (a-i-h)/5 +'px');
+    // $('.join-panel').css('background-position','center ' + (j-i-h)/5 +'px');
   });
 
   $('.home-link').click(function(){
     var  i = 0;
     $('body, html').animate({scrollTop: i - 70}, 1000);
+    $('nav').css('right','-200px');
+      $('body').css('right','0px');
+      $('header').css('right','0px');
+      nav = false;
   });
   $('.about-link').click(function(){
     var  i = $('.about').offset().top;
     $('body, html').animate({scrollTop: i - 70}, 1000);
+    $('nav').css('right','-200px');
+      $('body').css('right','0px');
+      $('header').css('right','0px');
+      nav = false;
   });
   $('.activities-link').click(function(){
     var  i = $('.activities-panel').offset().top;
     $('body, html').animate({scrollTop: i - 70}, 1000);
+    $('nav').css('right','-200px');
+      $('body').css('right','0px');
+      $('header').css('right','0px');
+      nav = false;
   });
   $('.join-link').click(function(){
     var  i = $('.join-panel').offset().top;
     $('body, html').animate({scrollTop: i - 70}, 1000);
+    $('nav').css('right','-200px');
+      $('body').css('right','0px');
+      $('header').css('right','0px');
+      nav = false;
   });
   $('.connect-link').click(function(){
     var  i = $('.contact').offset().top;
     $('body, html').animate({scrollTop: i - 70}, 1000);
+    $('nav').css('right','-200px');
+      $('body').css('right','0px');
+      $('header').css('right','0px');
+      nav = false;
+  });
+
+  nav = false;
+  $('.mobile-menu').click(function(){
+    if(nav){
+      $('nav').css('right','-200px');
+      $('body').css('right','0px');
+      $('header').css('right','0px');
+      nav = false;
+    }
+    else{
+      $('nav').css('right','0');
+      $('body').css('right','200px');
+      $('header').css('right','200px');
+      nav = true;
+    }
   });
 });
